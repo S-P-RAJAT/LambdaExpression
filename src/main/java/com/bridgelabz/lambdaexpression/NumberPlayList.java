7e84d19f81486aee8c8a5e0d8ceca704e01d1f77
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-14 01:06:31 +0530

    MODIFIED: Lambda funtion of two types, printing elements with foreach

--- src/main/java/com/bridgelabz/lambdaexpression/NumberPlayList.java
+++ src/main/java/com/bridgelabz/lambdaexpression/NumberPlayList.java
@@ -6,16 +6,22 @@ import java.util.function.Consumer;
 public class NumberPlayList {
 
     public static void main(String[] args) {
-        ArrayList<Integer> myNumberList=new ArrayList<Integer>();
-        for(int i=0;i<5;i++) {
+        ArrayList<Integer> myNumberList = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++)
             myNumberList.add(i);
-        }
-        for(int i:myNumberList) {
+
+        for (int i : myNumberList)
             System.out.println("numbers are " + i);
-        }
-        class MyConsumer implements Consumer<Integer>{
+        // explicit lambda function
+        Consumer<Integer> myListAction = n -> System.out.println("lambda impl foreach value :" + n);
+        myNumberList.forEach(myListAction);
+
+        // implicit lambda function
+        myNumberList.forEach(n -> System.out.println("values are: " + n));
+
+        class MyConsumer implements Consumer<Integer> {
             public void accept(Integer t) {
-                System.out.println("values of list by implementing consumer class: "+t);
+                System.out.println("values of list using class: " + t);
             }
         }
         MyConsumer action = new MyConsumer();
